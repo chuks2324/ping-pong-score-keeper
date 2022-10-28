34c1d6b962492991f2aa618c697ed124f68990b9
Chukwudi <54580449+chuks2324@users.noreply.github.com>
2022-10-28 21:36:15 +0100

    UI changes

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 const player1Score = document.querySelector(".player-1");
 const player2Score = document.querySelector(".player-2");
-
 const winningScore = document.querySelector("#end-score");
-
-console.log(winningScore);
 const controls = document.querySelectorAll(".footer-button");
 let p1Score = 0;
 let p2Score = 0;
@@ -13,20 +10,24 @@ let endScore = 3;
 controls.forEach((control) =>
   control.addEventListener("click", (e) => {
     if (isGameOver === false) {
-      if (e.target.id === "p1-score+1") {
+      if (e.target.id === "p1-score1") {
         player1Scoresfunction();
       }
-      if (e.target.id === "p2-score+1") {
+      if (e.target.id === "p2-score1") {
         player2Scoresfunction();
       }
       if (p1Score === endScore || p2Score === endScore) {
         isGameOver = true;
         if (p1Score === endScore) {
-          document.querySelector(".player-1").classList.add("text-success");
-          document.querySelector(".player-2").classList.add("text-danger");
+          player1Score.classList.add("text-success");
+          player2Score.classList.add("text-danger");
+          document.querySelector("#p1-score1").disabled = true;
+          document.querySelector("#p2-score1").disabled = true;
         } else {
-          document.querySelector(".player-1").classList.add("text-danger");
-          document.querySelector(".player-2").classList.add("text-success");
+          player1Score.classList.add("text-danger");
+          player2Score.classList.add("text-success");
+          document.querySelector("#p1-score1").disabled = true;
+          document.querySelector("#p2-score1").disabled = true;
         }
       }
     }
@@ -52,6 +53,8 @@ winningScore.addEventListener("change", (e) => {
   document
     .querySelector(".player-2")
     .classList.remove("text-success", "text-danger");
+  document.querySelector("#p1-score1").disabled = false;
+  document.querySelector("#p2-score1").disabled = false;
 });
 
 const player1Scoresfunction = function () {
@@ -69,4 +72,6 @@ const toReset = function () {
   p2Score = 0;
   player1Score.innerText = p1Score;
   player2Score.innerText = p2Score;
+  document.querySelector("#p1-score1").disabled = false;
+  document.querySelector("#p2-score1").disabled = false;
 };
